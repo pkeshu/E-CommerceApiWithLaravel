0b8c5785b399e349ce751d7c694ce79355b877a2
pkeshu <paudelkeshar2055@gmail.com>
2019-04-19 15:31:28 +0545

    created model for project

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\model\product;
+use Faker\Generator as Faker;
+
+$factory->define(product::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\model\review;
+use Faker\Generator as Faker;
+
+$factory->define(review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
