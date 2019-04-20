a9b3864b4fd4dc474f4e79153174a9144a85aa52
pkeshu <paudelkeshar2055@gmail.com>
2019-04-20 09:40:24 +0545

    seeding part for product and review in database is completed.

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -3,10 +3,16 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\model\review;
+use App\model\product;
 use Faker\Generator as Faker;
 
 $factory->define(review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function () {
+            return product::all()->random();
+        },
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0, 5)
     ];
 });
